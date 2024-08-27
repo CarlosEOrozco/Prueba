586651ececffb46838bfc6e7f423d10a052b7aff
carlos <alumno@920-M20>
2024-08-27 15:49:57 -0300

    inclusion nuevo SQL, con funcionalidades

--- Ejercicio_1-6/ConsoleApp1/Dominio/Product.cs
+++ Ejercicio_1-6/ConsoleApp1/Dominio/Product.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Datos
+namespace ConsoleApp1.Dominio
 {
-    internal class DataHelper
+    internal class Product
     {
     }
 }
